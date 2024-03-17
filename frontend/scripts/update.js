a30cb2be2328cea4b572fd5a50f29d9dc7d3e5bd
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-18 00:13:54 +0200

    added-update-script

--- frontend/scripts/update.js
+++ frontend/scripts/update.js
@@ -15,12 +15,12 @@ $(document).on('click', '.edit-btn', function(event) {
 });
 
 const editNews = async (id, title, description) => {
-    console.log(id);
     const formData = new FormData();
         formData.append('id', id);
         formData.append('new_title', title);
         formData.append('new_description', description);
     try {
+        console.log(formData);
         const result = await axios.post(
             "http://localhost/news-website/backend/update.php",
             formData,
@@ -51,53 +51,7 @@ editNewsButton.on('click', function() {
     const title = $("#edit-title");
     const description = $("#edit-description");
     editNews(id.val(), title.val(), description.val())
-   
 }); 
-
-// const addNews = async (title, description) => {
-//     const formData = new FormData();
-//         formData.append('title', title);
-//         formData.append('description', description);
-//     try {
-//         const result = await axios.post(
-//             "http://localhost/news-website/backend/create.php",
-//             formData,
-//             {
-//                 headers: {
-//                     "Content-Type": "multipart/form-data" 
-//                 }
-//             }
-//             );
-
-//         if (result.data.status == "Success")
-//         {
-//            container.html(container.html()+ `<div class="flex column center card secondary-bg" id="news-card">
-//            <img src="./assets/channels4_profile.jpg" alt="news" />
-//            <h3>${title}</h3>
-//            <p>${description}</p>
-//            <div class="flex gap">
-//                <button class="btn">Edit</button>
-//                <button class="btn">Delete</button>
-//            </div>
-//        </div>`) 
-//         }
-//         else
-//         {
-//             console.log("Error")
-//         }
-//         } catch (error) {
-//             console.log(error);
-//     }  };
-
-
-
-
-// addNewsButton.on('click', function() {
-//     const title = $("#title");
-//     const description = $("#description");
-//     addNews(title.val(), description.val())
-   
-// });
 
 
 
